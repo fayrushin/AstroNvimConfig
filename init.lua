232639b3de5253c0c41d093b89d7ef8893aee6f6
Ravil Fayrushin <ravil.fayrushin@gmail.com>
2022-10-14 08:57:25 +0000

    fix light color

--- init.lua
+++ init.lua
@@ -130,7 +130,7 @@ local config = {
     -- add new colors that can be used by heirline
     colors = {
       blank_bg = "#5c6370",
-      file_info_bg = "#3e4452",
+      file_info_bg = "#a3589c",
       nav_icon_bg = "#89b06d",
       folder_icon_bg = "#ec5f67",
     },
