d07cfb0da5218aa53224901c701e892bf67207da
Ravil <ravil@pop-os.localdomain>
2023-04-14 12:33:46 +0300

    rm wasted files

--- build_analytics_ide.sh
+++ build_analytics_ide.sh
@@ -2,7 +2,7 @@
 
 docker build --build-arg USER_ID="${UID:-1000}" \
   --build-arg GROUP_ID="${GID:-1000}" \
-  --build-arg BASE_IMAGE=base_analytics \
+  --build-arg BASE_IMAGE=analytics_base \
   --build-arg IMAGE_VERSION=latest \
   --build-arg GIT_USER_NAME="Ravil Fayrushin" \
   --build-arg GIT_USER_EMAIL="ravil.fayrushin@gmail.com" \
