ff41b8bd2b21fdd1bd711413cf00fde6fa500243
Ravil <ravil@pop-os.localdomain>
2023-05-30 01:09:05 +0300

    fix change in astronvim community

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,7 +4,7 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.nightfox" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
   -- { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
   { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
   -- { import = "astrocommunity.debugging.nvim-bqf" },
