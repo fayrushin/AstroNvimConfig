6a80c875facd0dda7e238166dde587a05ddcd566
Ravil Fayrushin <ravil.fayrushin@gmail.com>
2023-07-05 13:17:02 +0000

    fix indent bug

--- options.lua
+++ options.lua
@@ -9,12 +9,16 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
     smartindent = true,
+    autoindent = true,
+    -- copyindent = false, -- Copy the previous indentation on autoindenting
+    -- preserveindent = false, -- Preserve indent structure as much as possible
+    -- breakindent = false, -- Wrap indent to match  line start
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
     autoformat_enabled = false, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
     cmp_enabled = true, -- enable completion at start
-    autopairs_enabled = true, -- enable autopairs at start
+    autopairs_enabled = false, -- enable autopairs at start
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -2,5 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = { "lua", "cpp", "python", "cmake", "bash", "c", "json", "markdown", "dockerfile", "yaml" },
+    indent = { enable = false },
   },
 }
