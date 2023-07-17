e6c3f44f7559a68a4d5220ca640aca985f2f5035
Ravil Fayrushin <ravil.fayrushin@gmail.com>
2023-07-17 14:13:25 +0000

    format gitignored files

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,7 +2,8 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
+    local h = require "null-ls.helpers"
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -10,7 +11,14 @@ return {
     config.sources = {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/lua/null-ls/builtins/formatting/prettier.lua
+      null_ls.builtins.formatting.prettier.with {
+        args = h.range_formatting_args_factory({
+          "--stdin-filepath",
+          "--ignore-path=.prettierignore",
+          "$FILENAME",
+        }, "--range-start", "--range-end", { row_offset = -1, col_offset = -1 }),
+      },
     }
     return config -- return final config table
   end,
