cb783fae02e32e10f423a43289ce1310caa4321d
Ravil Fayrushin <ravil.fayrushin@gmail.com>
2023-10-05 12:47:59 +0300

    fix display on docker

--- run_analytics_ide.sh
+++ run_analytics_ide.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export DISPLAY=:1
 xhost +
-docker run -it --gpus all --privileged --cap-add=SYS_PTRACE -v "/home/ravil/.Xauthority":"/home/user/.Xauthority:rw"\
-  --env="DISPLAY" --net=host -v "/home/ravil/.ssh":"/home/user/.ssh" -v /media/work:/media/work --name=developer developer
+docker run -it --gpus all --privileged --cap-add=SYS_PTRACE \
+  -v /tmp/.X11-unix:/tmp/.X11-unix --env=DISPLAY  \
+  --net=host -v "/home/ravil/.ssh":"/home/user/.ssh" \
+  -v /home/ravil/Projects:/work --name=developer analytics_develop
