03939cb27209f57f1b5878d995374672e472b3fc
Файрушин Равил <r.fajrushin@dssl.ru>
2024-03-27 15:48:27 +0300

    increase file size from 100 KB to 1MB

--- options.lua
+++ options.lua
@@ -22,5 +22,6 @@ return {
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+    max_file = { size = 1024 * 1000, lines = 20000 }, -- set global limits for large files
   },
 }
